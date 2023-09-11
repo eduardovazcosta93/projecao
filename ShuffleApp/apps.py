2ec80e30863e059589a527f708bc4ae15029386e
Eduardo Vaz da Costa <eduardovazcosta93@gmail.com>
2023-09-11 16:50:34 -0300

    estruturação do projeto e começo da estruturação do frontend

--- ShuffleApp/apps.py
+++ ShuffleApp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SorteioAutomaticoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Sorteio_automatico'
+    name = 'ShuffleApp'
--- ShuffleApp/views.py
+++ ShuffleApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# HTTP REQUEST
+
+
+def home_page_view(request):
+    
+    list = {'nome':'Eduardo', 'sobrenome':'Vaz da Costa', 'data':'17/05/1993'}
+    # FUNÇÃO RETORNA HTTP RESPONSE
+    return render(request, 'ShuffleApp/home.html', context=list)
